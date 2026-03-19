7ea4337a079677ad2e973fc2e6d17575e1b21a92
p314dO <nicolasgula2023@gmail.com>
2026-03-18 21:51:39 -0300

    finish mvp

--- 01 - Python/03 - Python Projects - FZTH/01 - Smart Bill Calculator/app.py
+++ 01 - Python/03 - Python Projects - FZTH/01 - Smart Bill Calculator/app.py
@@ -1,5 +1,5 @@
 
-def validate_negative_values(value):
+def validate_values(value):
     if value < 0:
         return False
     else:
@@ -8,46 +8,54 @@ def validate_negative_values(value):
 def calculate_discount(price, discount):
     discount_amount = price * discount / 100
     final_price = price - discount_amount
-    return final_price
+    return [final_price , discount_amount]
 
-def calculate_iva(price, iva):
-    iva_amount = price * iva / 100
-    price_with_iva = price + iva_amount
-    return price_with_iva
+def calculate_vat(price, vat):
+    vat_amount = price * vat / 100
+    price_with_vat = price + vat_amount
+    return [price_with_vat , vat_amount]
 
-def calculate_tip():
-    return False
+def calculate_tip(price_with_vat, tip_percentage):
+    tip = price_with_vat * tip_percentage / 100
+    final_total = price_with_vat + tip
+    return [final_total, tip]
 
-def calcuate_full_buy():
-    return False
-
-def show_resume():
-    print("Hi")
+def show_summary(base_price,vat_percentage,tip_percentage, discount_percentage, price_after_discount, discount_amount, price_with_vat, vat_amount,final_total, tip):
+    print("Base price: ", base_price)
+    print("Discount percentage: ", discount_percentage)
+    print("VAT percentage: ", vat_percentage)
+    print("Tip percentage: ", tip_percentage)
+    print("Discount amount", discount_amount)
+    print("Price after discount", price_after_discount)
+    print("VAT Amount:" , vat_amount)
+    print("Price with VAT: ", price_with_vat)
+    print("Tip: ", tip)
+    print("Price with TIP: ", final_total)
+    
+    
 
 def main():
-    base_price = int(input("Insert the base price of the product: "))
+    base_price = float(input("Insert the base price of the product: "))
     
-    if(validate_negative_values(base_price)):
-        discount_percentage = int(input("Insert the discount percentage of the product: "))
+    if(validate_values(base_price)):
+        discount_percentage = float(input("Insert the discount percentage of the product: "))
         
-        if(validate_negative_values(discount_percentage)):
-            vat_percentage = int(input("Insert the VAT percentage of the product: "))
+        if(validate_values(discount_percentage)):
+            vat_percentage = float(input("Insert the VAT percentage of the product: "))
             
-            if(validate_negative_values(vat_percentage)):
-                tip_percentage = int(input("Insert the tip percentage of the product: "))
+            if(validate_values(vat_percentage)):
+                tip_percentage = float(input("Insert the tip percentage of the product: "))
                 
-                if(validate_negative_values(tip_percentage)):
+                if(validate_values(tip_percentage)):
                     # TODO Logic Business Here 
                     
-                    final_price = calculate_discount(base_price, discount_percentage)
+                    price_after_discount, discount_amount = calculate_discount(base_price, discount_percentage)
                     
-                    price_with_iva = calculate_iva(final_price, vat_percentage)
+                    price_with_vat, vat_amount = calculate_vat(price_after_discount, vat_percentage)
                     
-                    calculate_tip()
+                    final_total, tip = calculate_tip(price_with_vat, tip_percentage)
                     
-                    calcuate_full_buy()
-                    
-                    show_resume()
+                    show_summary(base_price,vat_percentage, tip_percentage,discount_percentage, price_after_discount, discount_amount,price_with_vat, vat_amount,final_total, tip )
                     
                 else:
                     print("The tip cannot be less than zero. Bye Bye")
